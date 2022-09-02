3db549c7cd5b651e7d8539d70f5d5424b00d99ea
Kovriga <60592893+Kovriga@users.noreply.github.com>
2022-09-02 12:22:43 +0700

    #3 Макет Main To Do листа

---
+++ src/components/MainComponentToDoList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component
+
+export default class MainComponentToDoList extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
